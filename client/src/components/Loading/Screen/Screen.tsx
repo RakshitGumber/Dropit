008c9d405b7a59314c5db29ef27d404c46fec1e6
RakshitGumber <gumberrakshit@gmail.com>
2025-05-30 11:01:09 +0530

    removed tailwind and usless stylings

--- client/src/components/Loading/Screen/Screen.tsx
+++ client/src/components/Loading/Screen/Screen.tsx
@@ -19,7 +19,7 @@ export const Screen = ({
           y: [0, -30, 0],
         }}
         transition={{
-          duration: 1~,
+          duration: 1,
           ease: "easeInOut",
         }}
         onAnimationComplete={() => {
