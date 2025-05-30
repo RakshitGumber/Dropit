dba56d2e5bd042e447d3ce578e8b078e5b0e375a
RakshitGumber <gumberrakshit@gmail.com>
2025-05-30 08:48:28 +0530

    ✨ feat(user): add new user attributes
    - adds email verification, password hashing, and profile information to the user model
    - includes full name, profile picture URL, and timestamps for creation and update

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,5 +5,6 @@ load_dotenv()
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
+
 if DATABASE_URL == None:
     raise ValueError("DATABASE_URL environment variable not set")
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,9 +1,15 @@
 from typing import Optional
 from sqlmodel import SQLModel, Field
+from datetime import datetime
 
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(index=True, nullable=False, unique=True)
     email: str = Field(nullable=False, unique=True)
-    is_active: bool = Field(default=True)
+    email_verified: bool = Field(default=False)
+    password_hashed: str = Field(nullable=False)
+    full_name: Optional[str] = None
+    profile_pic_url: Optional[str] = None
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
