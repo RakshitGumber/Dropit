cd7c27197fe7081e476a4321928d69e43c5a50e1
RakshitGumber <gumberrakshit@gmail.com>
2025-06-18 06:28:14 +0530

    added waitlist form

--- client/src/routes/(waitlist)/preregister/route.tsx
+++ client/src/routes/(waitlist)/preregister/route.tsx
@@ -1,9 +1,33 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import "./preregister.scss";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
-export const Route = createFileRoute('/(waitlist)/preregister')({
+export const Route = createFileRoute("/(waitlist)/preregister")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/preregister"!</div>
+  const navigate = useNavigate();
+  return (
+    <main className="preregister">
+      <button className="backbutton" onClick={() => navigate({ to: "/" })}>
+        <Icon icon="fa:arrow-left" />
+      </button>
+      <div className="form-container">
+        <h1>Join the Waitlist</h1>
+        <form>
+          <div>
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" placeholder="JohnDoe" />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" placeholder="jdoe@example.com" />
+          </div>
+          <span>Join waitlist to get future updates</span>
+          <button>Join</button>
+        </form>
+      </div>
+    </main>
+  );
 }
